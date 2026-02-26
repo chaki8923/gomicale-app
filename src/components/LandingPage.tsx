cfd3cc172b3f111b3e09bb1018219b607c6c4147
memo <konkuriitonouenokareha128@gmail.com>
2026-02-26 17:03:36 +0900

    広告追加2

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -196,7 +196,7 @@ function CtaSlide() {
         </div>
       </div>
 
-      <div className="w-full px-4 pb-2">
+      <div className="w-full max-h-[120px] overflow-hidden px-4 pb-2">
         <AdBanner slot="3248117735" />
       </div>
 
