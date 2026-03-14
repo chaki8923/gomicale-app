503259af1e2a409f2b9e75a04703391235387885
chaki8923 <aowp.contact@gmail.com>
2026-03-14 12:26:31 +0900

    広告変更

--- src/lib/ads.ts
+++ src/lib/ads.ts
@@ -15,14 +15,14 @@ export const ADS = {
     },
   },
   new_300x250: {
-    href: 'https://px.a8.net/svt/ejp?a8mat=4AZES3+G53376+2VIY+626XT',
+    href: 'https://px.a8.net/svt/ejp?a8mat=4AZES5+BDM8Z6+5KW8+5YZ75',
     image: {
-      src: 'https://www26.a8.net/svt/bgt?aid=260312979976&wid=001&eno=01&mid=s00000013417001018000&mc=1',
+      src: 'https://www23.a8.net/svt/bgt?aid=260312981688&wid=001&eno=01&mid=s00000026036001003000&mc=1',
       width: 300,
       height: 250,
     },
     tracking: {
-      src: 'https://www18.a8.net/0.gif?a8mat=4AZES3+G53376+2VIY+626XT',
+      src: 'https://www14.a8.net/0.gif?a8mat=4AZES5+BDM8Z6+5KW8+5YZ75',
     },
   },
   tall_120x600: {
